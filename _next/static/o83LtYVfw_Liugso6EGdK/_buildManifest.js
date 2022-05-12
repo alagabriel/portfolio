f1e309c5f21b41b09f07ec0aa72ae2d7bef79140
alagabriel <alagabriel.consulting@gmail.com>
2022-05-12 04:58:35 +0300

    single page test

---
+++ _next/static/o83LtYVfw_Liugso6EGdK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/876-a287ec6ade2627b6.js","static/css/e4dfa05f741a7677.css","static/chunks/pages/index-bce031f3060a3624.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
